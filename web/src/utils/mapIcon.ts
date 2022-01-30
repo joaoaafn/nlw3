5c117b1911230a64d60c7556197047cc3bd2aa07
joaoaafn <joao.aafn@gmail.com>
2022-01-30 16:11:20 -0300

    go

--- web/src/utils/mapIcon.ts
+++ web/src/utils/mapIcon.ts
@@ -1,6 +1,6 @@
 import Leaflet from 'leaflet';
 
-import mapMarkerImg from '../images/map-marker.svg';
+import mapMarkerImg from '../images/mapMarkerImg';
 
 const mapIcon = Leaflet.icon({
     iconUrl: mapMarkerImg,
